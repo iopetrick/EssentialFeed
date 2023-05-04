7ac36f597c16d20a83430a89644004bd578007c0
Pratik Prajapati <pratikprajapatisci@gmail.com>
2023-05-04 15:30:05 +0530

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Pratikkumar Prajapati on 04/05/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
