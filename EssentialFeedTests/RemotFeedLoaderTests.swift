9b03bb6011f85fb9d2355d624f6fedd02d6faf5c
Pratik Prajapati <pratikprajapatisci@gmail.com>
2023-05-05 15:34:48 +0530

    Create `HTTPClient` protocol

--- EssentialFeedTests/RemotFeedLoaderTests.swift
+++ EssentialFeedTests/RemotFeedLoaderTests.swift
@@ -8,20 +8,25 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
     func load() {
-        HTTPClient.shared.load(from: URL(string: "https://a-url.com")!)
+        client.load(from: URL(string: "https://a-url.com")!)
     }
 }
 
-class HTTPClient {
-    static var shared = HTTPClient()
-    func load(from url: URL) {}
+protocol HTTPClient {
+    func load(from url: URL)
 }
 
 class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
     
-    override func load(from url: URL) {
+    func load(from url: URL) {
         requestedURL = url
     }
 }
@@ -30,8 +35,7 @@ final class RemotFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        _ = RemoteFeedLoader()
+        _ = RemoteFeedLoader(client: client)
                         
         XCTAssertNil(client.requestedURL)
     }
@@ -39,8 +43,7 @@ final class RemotFeedLoaderTests: XCTestCase {
     func test_load_requestDataFromURL() {
         // arrange part
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let sut = RemoteFeedLoader()
+        let sut = RemoteFeedLoader(client: client)
         
         // Act part
         sut.load()
